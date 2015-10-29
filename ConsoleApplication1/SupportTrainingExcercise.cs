fb786a44d555dd1d8a36fca93ab5305cbe3dc827
Duff773 <Duff773@gmail.com>
2015-10-29 15:03:15 -0230

    Exercise for Gander
    basic exercise and project files, to run click ConsoleApplication1

---
+++ ConsoleApplication1/SupportTrainingExcercise.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 1; i < 101; i++)
+            {
+                if (i % 5 == 0 && i % 3 == 0)
+                {
+                    Console.WriteLine("SupportTraining");
+                }
+                else if (i % 5 == 0)
+                {
+                    Console.WriteLine("Training");
+                }
+                else if (i % 3 == 0)
+                {
+                    Console.WriteLine("Support");
+                }
+                else
+                {
+                    Console.WriteLine(i);
+                }
+            }
+            Console.ReadLine();
+        }
+    }
+}
